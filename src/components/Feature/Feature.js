0d4d58d11321d523fd19fddd1ef9388510ad837c
Liliripe <liliripe.studio@gmail.com>
2020-11-08 17:41:23 -0600

    Init feature grid

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
+import Wrapper from '../Wrapper'
 import Grid from './Grid'
 import Banner from './Banner'
 import {  } from './style'
 
 const Feature = ({ grid, number }) => {
   return (
-    <>
-      {grid ? (
-        <Grid number />
-      ) : (
-        <Banner />
-      )} 
-    </>
+    <Wrapper>
+      <Container fluid>
+        <Row>
+          {grid ? (
+            <Grid number />
+          ) : (
+            <Banner />
+          )} 
+        </Row>
+      </Container>
+    </Wrapper>
   )
 }
 
