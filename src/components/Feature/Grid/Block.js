d8501ebcfdfd86aadb41468350fd0a0e3ed2d495
Liliripe <liliripe.studio@gmail.com>
2020-11-11 16:59:58 -0600

    init page transitions

--- src/components/Feature/Grid/Block.js
+++ src/components/Feature/Grid/Block.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import { useInView } from 'react-intersection-observer'
+import { motion, useAnimation } from 'framer-motion'
 
 import { 
   StyledCol, 
@@ -13,36 +15,56 @@ import {
 } from './style'
 
 const FeatureGrid = ({ title, slug, categories, image }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+      if (inView) {
+      controls.start("visible");
+      }
+  }, [controls, inView]);
+
   return (
     <StyledCol lg="3" key={slug}>
-      <StyledBackground
-          Tag="div"
-          fluid={image}
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={{
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 }
+        }}
+        transition={{ duration: 1.3 }}
       >
-        <StyledLink 
-          fade 
-          to={slug}
-          alt={title}
+        <StyledBackground
+            Tag="div"
+            fluid={image}
         >
-          <Overlay>
-            <StyledButton>View Details</StyledButton>
-          </Overlay>
-        </StyledLink>
-      </StyledBackground>
-
-      <StyledText>
-        <AniLink 
-          fade 
-          to={slug}
-          alt={title}
-        >
-          <StyledTitle>{title}</StyledTitle>
-        </AniLink>
+          <StyledLink 
+            fade 
+            to={slug}
+            alt={title}
+          >
+            <Overlay>
+              <StyledButton>View Details</StyledButton>
+            </Overlay>
+          </StyledLink>
+        </StyledBackground>
   
-        {categories.slice(1, 2).map(category => (
-          <StyledSubtitle>{category.name}</StyledSubtitle>
-        ))}
-      </StyledText>
+        <StyledText>
+          <AniLink 
+            fade 
+            to={slug}
+            alt={title}
+          >
+            <StyledTitle>{title}</StyledTitle>
+          </AniLink>
+    
+          {categories.slice(1, 2).map(category => (
+            <StyledSubtitle>{category.name}</StyledSubtitle>
+          ))}
+        </StyledText>
+      </motion.div>
     </StyledCol>
   )
 }
