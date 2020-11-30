884d45b60f320df5777f8a15ec4c956cd96c1116
Liliripe <liliripe.studio@gmail.com>
2020-11-30 14:58:14 -0600

    Stripped down Destinations template test

--- src/templates/destination.js
+++ src/templates/destination.js
@@ -1,58 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
 
-export const DestinationPostTemplate = ({ title }) => {
+export const DestinationPostTemplate = () => {
   return (
-    <section className="section">
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div>
+      TEST PAGE
+    </div>
   )
 }
 
-DestinationPostTemplate.propTypes = {
-  title: PropTypes.string,
-}
-
-const DestinationPost = ({ data }) => {
-  const { wordpressWpDestinations: post } = data
-
+const DestinationPost = () => {
   return (
     <>
-      <Helmet title={`${post.title} | Destinations`} />
-      <DestinationPostTemplate
-        title={post.title}
-      />
+      <Helmet title={`Test | Destinations`} />
+      <DestinationPostTemplate />
     </>
   )
 }
-
-DestinationPost.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
-
 export default DestinationPost
-
-export const pageQuery = graphql`
-  fragment DestinationFields on wordpress__POST {
-    id
-    title
-  }
-  query DestinationPostByID($id: String!) {
-    wordpressWpDestinations(id: { eq: $id }) {
-      id
-      title
-    }
-  }
-`
