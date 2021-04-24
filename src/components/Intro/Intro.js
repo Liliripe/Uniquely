3852a46c27b5acba0d42f8097ad5d80071454999
Liliripe <liliripe.studio@gmail.com>
2021-04-24 12:40:19 -0500

    intro component update

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,39 +1,50 @@
 import React, { useEffect } from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import { useInView } from 'react-intersection-observer'
 import { motion, useAnimation } from 'framer-motion'
 
 import { StyledIntro } from './style'
 
-const Intro = ({ title, align, padding, background, children }) => {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
+const Intro = ({ align, padding, background }) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
 
-  useEffect(() => {
-      if (inView) {
-      controls.start("visible");
-      }
-  }, [controls, inView]);
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
 
     return (
-        <StyledIntro>
-            <motion.div
-                ref={ref}
-                animate={controls}
-                initial="hidden"
-                variants={{
-                    visible: { opacity: 1 },
-                    hidden: { opacity: 0 }
-                }}
-                transition={{ duration: 1.3 }}
-                className="intro-wrapper"
-                style={{ textAlign: align, padding: padding, backgroundColor: background }}
-            >
-                <div class="inner-content">
-                    <h2>{title}</h2>
-                    <div dangerouslySetInnerHTML={{ __html: children }} />
-                </div>
-            </motion.div>
-        </StyledIntro>
+        <StaticQuery
+            query={graphql`
+                query {
+                    wordpressPage(title: {eq: "Home"}) {
+                        content
+                    }
+                }
+            `}
+            render={data => (
+                <StyledIntro>
+                    <motion.div
+                        ref={ref}
+                        animate={controls}
+                        initial="hidden"
+                        variants={{
+                            visible: { opacity: 1 },
+                            hidden: { opacity: 0 }
+                        }}
+                        transition={{ duration: 1.3 }}
+                        className="intro-wrapper"
+                        style={{ textAlign: align, padding: padding, backgroundColor: background }}
+                    >
+                        <div class="inner-content">
+                            <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
+                        </div>
+                    </motion.div>
+                </StyledIntro>
+            )}
+        />
     )
 }
 
--- src/templates/index.js
+++ src/templates/index.js
@@ -13,13 +13,10 @@ export default class IndexPage extends React.Component {
       <Helmet title="Uniquely.io" />
         <Carousel type="hero" limit="5" />
         <Intro
-          title="Uniquely Intro Text Here"
           align="center"
           padding="100px 0"
           background="#f7f7f7"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at lectus justo. Phasellus maximus mauris et rutrum egestas. Sed venenatis mi accumsan nunc sollicitudin, nec ultrices metus elementum. Nam feugiat mauris id ante mattis, et euismod justo porttitor. Vivamus ligula est, congue eget mauris ut, commodo consectetur elit.
-        </Intro>
+        />
         <Title
           title="Popular <em>Featured</em> Content" 
           subtitle="Destinations & Events" 
